3c4c768fe99bc8f07c03269b53718922c9035a3d
[Matias] <monteil.mathias@gmail.com>
2024-05-06 17:57:37 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Nom from './component/Nom'
+
+
+const getNames = (firstName, secondName, thirdName) => {
+  return firstName + " " + secondName + " " + thirdName;
+}
+
+const Person = (props) => {
+  return (
+    <View>
+      <Text>Je suis : {props.name}</Text>
+    </View>
+  )
+}
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Hi guy</Text>
-      <StatusBar style="auto" />
+      <View style={{padding:10, backgrounColor:'black'}}>
+        <Text style={styles.text}>Hi guy</Text>
+        <Text style={styles.text}>What's up ?</Text>
+        <Text style={styles.text}>Can i help you ?</Text>
+        <Text style={{textAlign:"center", 
+                      fontStyle:"italic",
+                      fontSize:25}}
+          >
+            {getNames("Jean","Claude","Van Dame")}
+            <Person name= "Jeremie" />
+
+            <Nom Name= "James"/>
+            <Nom Name= "Julie" Age="18"/>
+
+            <Nom Name='Mike'Age="15"/>
+          </Text>
+        <StatusBar style="auto" />
+        <Button
+            title="Learn More"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />     
+        </View>
     </View>
   );
 }
@@ -13,8 +50,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    marginBottom: '10%',
+    fontSize: 40,
   },
 });
